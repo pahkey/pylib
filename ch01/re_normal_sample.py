528ce50e5c62f2022c9778e8d423f889ec69cbd2
박응용 <pahkey@gmail.com>
2021-07-12 17:09:43 +0900

    ch01

---
+++ ch01/re_normal_sample.py
@@ -0,0 +1,15 @@
+data = """
+홍길동의 주민번호는 800905-1049118 입니다. 
+그리고 고길동의 주민번호는 700905-1059119 입니다.
+그렇다면 누가 형님일까요?
+"""
+
+result = []
+for line in data.split("\n"):
+    word_result = []
+    for word in line.split(" "):
+        if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit():
+            word = word[:6] + "-" + "*******"
+        word_result.append(word)
+    result.append(" ".join(word_result))
+print("\n".join(result))
