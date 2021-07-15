164c3e6e94e879c52b2ae4f40f46c2ef21d3a808
박응용 <pahkey@gmail.com>
2021-07-16 08:50:21 +0900

    patch

--- ch18/requests_patch_sample.py
+++ ch18/requests_patch_sample.py
@@ -5,7 +5,7 @@ import json
 url = 'https://jsonplaceholder.typicode.com/posts/1'
 headers = {'Content-type': 'application/json; charset=utf-8'}
 data = {
-    'title': 'Hello',
+    'title': 'foo',
 }
 res = requests.patch(url, headers=headers, data=json.dumps(data))
 pprint.pprint(res.json())
